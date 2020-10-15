6a27672fb25e15f839b1145e88f5ec6faee7a8b7
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-10-15 14:11:39 +0530

    Update scu_functions.cpp

--- scutest/scu_functions.cpp
+++ scutest/scu_functions.cpp
@@ -69,3 +69,87 @@ void scu::CopyPortName(int* i, string A_argv[], STORAGE_OPTIONS* A_options)
 
 	}
 }
+SAMP_BOOLEAN scu::traverseimages(STORAGE_OPTIONS* A_options, InstanceNode** instanceList, char fname[])
+{
+	SAMP_BOOLEAN sampBool = SAMP_FALSE;
+
+	int imageCurrent = 0;
+	for (imageCurrent = A_options->StartImage; imageCurrent <= A_options->StopImage; imageCurrent++)
+	{
+		sprintf(fname, "%d.img", imageCurrent);
+		sampBool = scu::AddFileToList(&(*instanceList), fname);
+		
+	}
+	return sampBool;
+}
+ SAMP_BOOLEAN scu::AddFileToList(InstanceNode** A_list, char* A_fname)
+{
+	InstanceNode*    newNode;
+	
+
+	newNode = (InstanceNode*)malloc(sizeof(InstanceNode));
+	if (!newNode)
+	{
+		//PrintError("Unable to allocate object to store instance information", MC_NORMAL_COMPLETION);
+		return (SAMP_FALSE);
+	}
+
+	memset(newNode, 0, sizeof(InstanceNode));
+
+	strncpy(newNode->fname, A_fname, sizeof(newNode->fname));
+	newNode->fname[sizeof(newNode->fname) - 1] = '\0';
+
+	newNode->responseReceived = SAMP_FALSE;
+	newNode->failedResponse = SAMP_FALSE;
+	newNode->imageSent = SAMP_FALSE;
+	newNode->msgID = -1;
+	newNode->transferSyntax = IMPLICIT_LITTLE_ENDIAN;
+
+	scu::update_list(A_list, newNode);
+
+	return (SAMP_TRUE);
+}
+void scu::update_list(InstanceNode** A_list, InstanceNode* newNode)
+{
+	InstanceNode* listNode;
+	if (!*A_list)
+	{
+		/*
+
+		  * Nothing in the list
+
+		  */
+		newNode->Next = *A_list;
+
+		*A_list = newNode;
+
+	}
+	else
+	{
+		/*
+
+		 * Add to the tail of the list
+
+		 */
+		listNode = *A_list;
+		while (listNode->Next)
+			listNode = listNode->Next;
+		listNode->Next = newNode;
+
+	}
+
+}
+int scu::GetNumNodes(InstanceNode*       A_list)
+{
+	int            numNodes = 0;
+	InstanceNode*  node;
+
+	node = A_list;
+	while (node)
+	{
+		numNodes++;
+		node = node->Next;
+	}
+
+	return numNodes;
+}
