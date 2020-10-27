78998b653741e867dd156a8c3546fa154331abca
SaloniMadhan <69685422+SaloniMadhan@users.noreply.github.com>
2020-10-27 13:05:43 +0530

    Update ImageTrackerTest.py

--- ImageTracker/ImageTrackerTest.py
+++ ImageTracker/ImageTrackerTest.py
@@ -1,6 +1,7 @@
 import unittest
 import ImageTracker
 
+
 class TestImageTracker(unittest.TestCase):   
     def test_when_client_calls_with_valid_csv_then_print_CSV_data(self):
         testsamplefilename = 'temp-samples-test.csv'
@@ -16,15 +17,29 @@ class TestImageTracker(unittest.TestCase):
 
 
     def test_when_client_updates_Diagnosis_status_Pending_then_DiagnosisPending_is_set(self):
+        testsamplefilename = 'temp-samples-test.csv'
+        with open(testsamplefilename, 'w') as samplefile:            
+            samplefile.write(
+"""ImageFileName,Status
+0.img,Diagnosis Pending
+1.img,Diagnosis Pending
+2.img,Diagnosis Pending""")
         imageName="0.img"
         updated_status="Pending"   # Pending for Diagnosis Pending
-        self.assertTrue(ImageTracker.update_ImageStatus(imageName,updated_status)==True)
+        self.assertTrue(ImageTracker.update_ImageStatus(testsamplefilename,imageName,updated_status)==True)
 
 
     def test_when_client_updates_Diagnosis_status_Pending_then_DiagnosisCompleted_is_set(self):
+        testsamplefilename = 'temp-samples-test.csv'
+        with open(testsamplefilename, 'w') as samplefile:            
+            samplefile.write(
+"""ImageFileName,Status
+0.img,Diagnosis Pending
+1.img,Diagnosis Pending
+2.img,Diagnosis Pending""")
         imageName="1.img"
         updated_status="Completed"   # Complete for Diagnosis Complete
-        self.assertTrue(ImageTracker.update_ImageStatus(imageName,updated_status)==True)
+        self.assertTrue(ImageTracker.update_ImageStatus(testsamplefilename,imageName,updated_status)==True)
 
 
     def test_when_new_file_is_updated_then_it_gets_added_to_csv_with_Pending_Status(self):
@@ -41,5 +56,6 @@ class TestImageTracker(unittest.TestCase):
         x=ImageTracker.showCSVData(testsamplefilename)
         self.assertEqual(x,"CSV data printed successfully")   
 
+        
 if __name__ == '__main__':    
     unittest.main()
